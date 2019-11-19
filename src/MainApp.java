6c55a98213bd5584ad5ba25584f47186c5382410
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-11-19 20:25:55 +0800

    登录页面跳转完成

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,67 +1,49 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
-import view.loginProgressBarController;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Date;
-import java.util.Properties;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
-    private BorderPane loginMainUIPane;
-    private AnchorPane loginUIPane;
-    private AnchorPane loginProgressBar;
+    private AnchorPane progressBarPane;
+    private BorderPane mainPane;
 
-    public void initLoginMainUIPane() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/loginMainUI.fxml"));
-            loginMainUIPane = loader.load();
-
-            Scene scene = new Scene(loginMainUIPane);
-            new JMetro(scene, Style.LIGHT);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
+    Service<Integer> service = new Service<Integer>() {
+        @Override
+        protected Task<Integer> createTask() {
+            return new Task<Integer>() {
+                @Override
+                protected Integer call() throws Exception {
+                    //---------------------------------
+                    //临时代码，替换连接数据库操作
+                    for (int i = 0; i < 30; i++) {
+                        Thread.sleep(100);
+                        System.out.println(i + 1);
+                    }
+                    //---------------------------------
+                    Platform.runLater(()->{
+                        primaryStage.hide();
+                        try {
+                            showMainPane();//跳转至主页面
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    return null;
+                }
+            };
         }
-    }//初始化登录页面容器
-
-    public void showLoginUIPane() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/loginUI.fxml"));
-            loginUIPane = loader.load();
-            loginMainUIPane.setCenter(loginUIPane);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }//登录页面主页面（账号密码输入页面）
-
-    public void showLoginProgressBar() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/loginProgressBar.fxml"));
-            loginProgressBar = loader.load();
-            loginMainUIPane.setCenter(loginProgressBar);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }//登陆页面数据库连接进度条
+    };
 
     public static void main(String[] args) {
         launch(args);
@@ -70,9 +52,40 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("登录");
-        initLoginMainUIPane();
-
+        this.primaryStage.setTitle("食品公司管理系统");
         showLoginProgressBar();
     }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void showLoginProgressBar() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(MainApp.class.getResource("view/loginProgressBar.fxml"));
+            progressBarPane = (AnchorPane) fxmlLoader.load();
+
+            Scene scene = new Scene(progressBarPane);
+            new JMetro(scene, Style.LIGHT);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            service.restart();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }//加载页面
+
+    public void showMainPane() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(MainApp.class.getResource("view/mainWindow.fxml"));
+        mainPane = (BorderPane) fxmlLoader.load();
+
+        Scene scene = new Scene(mainPane);
+        new JMetro(scene, Style.LIGHT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }//主页面
 }
+
+
--- src/view/loginProgressBarController.java
+++ src/view/loginProgressBarController.java
@@ -19,4 +19,6 @@ public class loginProgressBarController {
         connectDBProgress.setProgress(-1);
     }
     //TODO
+
+
 }
