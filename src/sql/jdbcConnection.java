0fe553e3812b097a36035d837b407bdd5b114131
Abrahamchen <530153667@qq.com>
2019-11-30 16:29:05 +0800

    新建了简略验证函数 尚未搞懂如何接收前端数据并进行处理和返回结果 尚未搞懂如何进行跨包调用类中函数 包之间的通信原理

--- src/sql/jdbcConnection.java
+++ src/sql/jdbcConnection.java
@@ -1,4 +1,4 @@
-package Sql;
+package sql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
--- src/sql/sss.java
+++ src/sql/sss.java
@@ -1,4 +1,4 @@
-package Sql;
+package sql;
 
 public class sss {
     //sql包用于连接数据库，执行sql语句，获得结果集，返回给DAO层，DAO层返回给业务逻辑层。业务逻辑层返回给界面层。
