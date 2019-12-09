2c1aecf1a9d9ab7164a7633aae6fcd0459345360
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-12-09 21:13:38 +0800

    登录连接数据库BUG已解决

--- src/service/UserInfoCheck.java
+++ src/service/UserInfoCheck.java
@@ -11,29 +11,27 @@ import org.hibernate.Transaction;
 //验证用户的number和密码是否合法 -1.用户名不存在 0.密码错误 1.成功
 public class UserInfoCheck {
 
-    public int isValidNumber(String number, String pw)
-    {
-        int res=1;
+    public int isValidNumber(String number, String pw) {
+        int res = 1;
         Session session = HibernateUtils.openSession();
-        Transaction tx=null;
+        Transaction tx = null;
 
         try {
 
-//            tx=session.beginTransaction();
-//            Stuff stuff =(Stuff) session.get(Stuff.class,number);
-//            System.out.println("_____________________________________"+stuff.getPassword().toString());
-//            if(stuff.getPassword().toString().equals(pw)) {
-//                res=0;
-//                System.out.println("_____________________________________"+res);
-//
-//            }
-//            else if(!stuff.getPassword().toString().equals(pw)) res=2;
-//            tx.commit();
-        }catch (RuntimeException e){
+            tx = session.beginTransaction();
+            Stuff stuff = (Stuff) session.get(Stuff.class, Integer.valueOf(number));
+            System.out.println("_____________________________________" + stuff.getPassword().toString());
+            if (stuff.getPassword().toString().equals(pw)) {
+                res = 0;
+                System.out.println("_____________________________________" + res);
+
+            } else if (!stuff.getPassword().toString().equals(pw)) res = 2;
+            tx.commit();
+        } catch (RuntimeException e) {
             tx.rollback();
             System.out.println("_____________________________ERROR______________________________________");
             throw e;
-        }finally {
+        } finally {
             session.close();
             System.out.println("_____________________________CLOSE______________________________________");
             return res;
