aa1cc5ca057624a144682c02cf00a9c9fb085dd0
Abrahamchen <530153667@qq.com>
2019-12-11 17:28:56 +0800

    添加了商品库表和成品库表 已插入字段 可尝试

---
+++ src/model/Goods.java
@@ -0,0 +1,87 @@
+package model;
+
+import javafx.beans.property.*;
+import javafx.css.SimpleStyleableIntegerProperty;
+
+import java.time.LocalDateTime;
+
+//商品表
+public class Goods {
+    private IntegerProperty goodsId =new SimpleIntegerProperty();
+    private StringProperty goodsName=new SimpleStringProperty();
+    private DoubleProperty goodsPrice =new SimpleDoubleProperty();
+    private IntegerProperty goodsQualityTime = new SimpleIntegerProperty();
+    private IntegerProperty requiredQuantity = new SimpleIntegerProperty();
+    //private ObjectProperty<LocalDateTime> = new ObjectProperty<LocalDateTime>
+
+    public int getGoodsId() {
+        return goodsId.get();
+    }
+
+    public IntegerProperty goodsIdProperty() {
+        return goodsId;
+    }
+
+    public void setGoodsId(int goodsId) {
+        this.goodsId.set(goodsId);
+    }
+
+    public String getGoodsName() {
+        return goodsName.get();
+    }
+
+    public StringProperty goodsNameProperty() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName.set(goodsName);
+    }
+
+    public double getGoodsPrice() {
+        return goodsPrice.get();
+    }
+
+    public DoubleProperty goodsPriceProperty() {
+        return goodsPrice;
+    }
+
+    public void setGoodsPrice(double goodsPrice) {
+        this.goodsPrice.set(goodsPrice);
+    }
+
+    public int getGoodsQualityTime() {
+        return goodsQualityTime.get();
+    }
+
+    public IntegerProperty goodsQualityTimeProperty() {
+        return goodsQualityTime;
+    }
+
+    public void setGoodsQualityTime(int goodsQualityTime) {
+        this.goodsQualityTime.set(goodsQualityTime);
+    }
+
+    public int getRequiredQuantity() {
+        return requiredQuantity.get();
+    }
+
+    public IntegerProperty requiredQuantityProperty() {
+        return requiredQuantity;
+    }
+
+    public void setRequiredQuantity(int requiredQuantity) {
+        this.requiredQuantity.set(requiredQuantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "goodsId=" + goodsId +
+                ", goodsName=" + goodsName +
+                ", goodsPrice=" + goodsPrice +
+                ", goodsQualityTime=" + goodsQualityTime +
+                ", requiredQuantity=" + requiredQuantity +
+                '}';
+    }
+}
