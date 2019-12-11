574fc1b2691d18a4bf91cc10c52069d79dd80104
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-12-11 22:37:52 +0800

    编写页面

--- src/view/ProductionManagePaneController.java
+++ src/view/ProductionManagePaneController.java
@@ -1,4 +1,48 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class ProductionManagePaneController {
+    @FXML
+    private TextField searchInputText;
+    @FXML
+    private Button searchBtn;
+    @FXML
+    private Button searchAllBtn;
+    @FXML
+    private TableView planList;
+    @FXML
+    private Label planNumberLabel;
+    @FXML
+    private Label numberLabel;
+    @FXML
+    private Label typeLabe;
+    @FXML
+    private Label cycleLebal;
+    @FXML
+    private Button uploadBtn;
+
+    @FXML
+    private void initialize() {
+        //TODO
+    }
+
+    @FXML
+    private void handleSearch() {
+        //TODO
+    }
+
+    @FXML
+    private void handleSearchAll() {
+        //TODO
+    }
+
+    @FXML
+    private void handleUpload() {
+        //TODO
+    }
 }
