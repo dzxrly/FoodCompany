da340d7b69c3c5aba763eed2bd76a013fcaaa88d
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-11 20:34:36 +0800

    Service包中添加了GoodsSearch类  实现商品的查询         返回值为List 可以进行模糊查询和 查询全部

--- src/service/GoodsSearch.java
+++ src/service/GoodsSearch.java
@@ -9,35 +9,37 @@ import java.util.List;
 
 public class GoodsSearch {
 
-    public List searchAllGoods() {
+    public List searchGoods(String name) {
         Session session = HibernateUtils.openSession();
         Transaction tx = null;
-        List list=null;
-
+        List list = null;
+        String hql = null;
         try {
-            tx=session.beginTransaction();
-            String hql="select g.goodsId,g.goodsName,g.goodsPrice,g.goodsQualityTime,s.stocks from Goods as g,ShippingDepartment as s where g.goodsId=s.goodsId";
-            Query query=session.createQuery(hql);
-            list=query.list();
+            tx = session.beginTransaction();
+            if (name != null)
+                hql = "select g.goodsId,g.goodsName,g.goodsPrice,g.goodsQualityTime,s.stocks from Goods as g,ShippingDepartment as s where g.goodsId=s.goodsId and g.goodsName like '%" + name + "%'";
+            else
+                hql = "select g.goodsId,g.goodsName,g.goodsPrice,g.goodsQualityTime,s.stocks from Goods as g,ShippingDepartment as s where g.goodsId=s.goodsId";
 
-//            for(int i=0;i<list.size();i++){
-////                Object[] obj=(Object[]) list.get(i);
-////
-////                String s1=((Integer)obj[0]).toString();//goodsId
-////                String s2=(String)obj[1];//goodsName
-////                String s3=((Double)obj[2]).toString();//goodsPrice
-////                String s4=((Integer)obj[3]).toString();//goodsQualityTime
-////                String s5=((Integer)obj[4]).toString();//stocks
-////
-////                System.out.println(s1+s2+s3+s4+s5);
-////            }
+            Query query = session.createQuery(hql);
+            list = query.list();
+
+//            for (int i = 0; i < list.size(); i++) {
+//                Object[] obj = (Object[]) list.get(i);
+//
+//                String s1 = ((Integer) obj[0]).toString();//goodsId
+//                String s2 = (String) obj[1];//goodsName
+//                String s3 = ((Double) obj[2]).toString();//goodsPrice
+//                String s4 = ((Integer) obj[3]).toString();//goodsQualityTime 保质期
+//                String s5 = ((Integer) obj[4]).toString();//stocks
+//
+//                System.out.println(s1 + s2 + s3 + s4 + s5);
+//            }
         } catch (RuntimeException e) {
             System.out.println("_____________Can not_____________________");
         } finally {
             session.close();
             return list;
         }
-
-
     }
 }
