d090ca97c01c74e6dd4f4775785e1033a87c2635
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-11 10:49:07 +0800

    Service包中添加了CustomerUpdate类 可以实现用户信息的更新 写了int型返回值

--- src/service/CustomerUpdate.java
+++ src/service/CustomerUpdate.java
@@ -9,7 +9,8 @@ import org.hibernate.query.Query;
 import java.util.List;
 
 public class CustomerUpdate {
-    public void updateCustomer(int number, String personalName, String companyName, Integer type, Integer level, String address, String email, String phoneNumber) {
+    public int updateCustomer(String personalName, String companyName, Integer type, Integer level, String address, String email, String phoneNumber, int number) {
+        int ans = 1;
         Session session = HibernateUtils.openSession();
         Transaction tx = null;
         Customer c = new Customer();
@@ -24,26 +25,27 @@ public class CustomerUpdate {
             tx = session.beginTransaction();
             String hql = "update Customer set personalName=:personalName,companyName=:companyName,type=:type ,level= :level,address= :address," +
                     "email=:email,phoneNumber=:phoneNumber where number=:number";
-            Query query = session.createQuery(hql);
+            Query query=session.createQuery(hql);
             query.setString("personalName", personalName);
             query.setString("companyName", companyName);
-            query.setInteger("type", type);
-            query.setInteger("level", level);
+            query.setInteger("type",type);
+            query.setInteger("level",level);
             query.setString("address", address);
             query.setString("email", email);
             query.setString("phoneNumber", phoneNumber);
-            query.setInteger("number", number);
+            query.setInteger("number",number);
             query.executeUpdate();
 
+
             tx.commit();
+            ans=1;//1表示更新成功
         } catch (RuntimeException e) {
-
             tx.rollback();
-
+            ans=0;//0表示更新失败
             System.out.println("__________________________Can't insert into table customer________________________");
-            throw e;
         } finally {
             session.close();
+            return ans;
         }
     }
 }
