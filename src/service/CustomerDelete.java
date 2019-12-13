677eb728265083e6540daf1a5f917c7b08dbee7c
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-13 11:39:15 +0800

    增加了CustomerDelete 在修改客户信息时可以根据客户number进行删除

---
+++ src/service/CustomerDelete.java
@@ -0,0 +1,31 @@
+package service;
+
+import DAO.HibernateUtils;
+import model.Customer;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class CustomerDelete {
+
+    public int deleteCustomer(int number){
+        int ans=0;
+        Session session = HibernateUtils.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            String hql="delete User as p where p.id=?";
+            Object user = session.get(Customer.class, number); // 要先获取到这个对象
+            session.delete(user); // 删除的是实体对象
+
+            ans=1;//1表示删除成功
+            tx.commit();
+        } catch (RuntimeException e) {
+            tx.rollback();
+            ans=0;//0表示删除失败
+            throw e;
+        } finally {
+            session.close();
+            return ans;
+        }
+    }
+}
