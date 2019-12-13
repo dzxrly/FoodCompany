93e1a81ca2e813f0e2471640a99f49f0569f39a1
Abrahamchen <530153667@qq.com>
2019-12-13 23:42:57 +0800

    update了退款记录表

--- src/model/ReturnRecord.java
+++ src/model/ReturnRecord.java
@@ -6,11 +6,8 @@ import javafx.beans.property.*;
 public class ReturnRecord {
     private IntegerProperty recordId = new SimpleIntegerProperty();
     private IntegerProperty orderId = new SimpleIntegerProperty();
-    private IntegerProperty orderType = new SimpleIntegerProperty();//0为现货订单 1为预定订单
     private IntegerProperty customerNumber = new SimpleIntegerProperty();
     private StringProperty customerName = new SimpleStringProperty();
-    private IntegerProperty customerLevel= new SimpleIntegerProperty(); //客户星级，用数字表示，分为1~5，5为最高.
-    private StringProperty returnTime = new SimpleStringProperty();
     private IntegerProperty stuffNumber =new SimpleIntegerProperty();
     private DoubleProperty totalReturnPrice = new SimpleDoubleProperty();//总退款金额
 
@@ -38,18 +35,6 @@ public class ReturnRecord {
         this.orderId.set(orderId);
     }
 
-    public int getOrderType() {
-        return orderType.get();
-    }
-
-    public IntegerProperty orderTypeProperty() {
-        return orderType;
-    }
-
-    public void setOrderType(int orderType) {
-        this.orderType.set(orderType);
-    }
-
     public int getCustomerNumber() {
         return customerNumber.get();
     }
@@ -72,30 +57,6 @@ public class ReturnRecord {
 
     public void setCustomerName(String customerName) {
         this.customerName.set(customerName);
-    }
-
-    public int getCustomerLevel() {
-        return customerLevel.get();
-    }
-
-    public IntegerProperty customerLevelProperty() {
-        return customerLevel;
-    }
-
-    public void setCustomerLevel(int customerLevel) {
-        this.customerLevel.set(customerLevel);
-    }
-
-    public String getReturnTime() {
-        return returnTime.get();
-    }
-
-    public StringProperty returnTimeProperty() {
-        return returnTime;
-    }
-
-    public void setReturnTime(String returnTime) {
-        this.returnTime.set(returnTime);
     }
 
     public int getStuffNumber() {
@@ -127,11 +88,8 @@ public class ReturnRecord {
         return "ReturnRecord{" +
                 "recordId=" + recordId +
                 ", orderId=" + orderId +
-                ", orderType=" + orderType +
                 ", customerNumber=" + customerNumber +
                 ", customerName=" + customerName +
-                ", customerLevel=" + customerLevel +
-                ", returnTime=" + returnTime +
                 ", stuffNumber=" + stuffNumber +
                 ", totalReturnPrice=" + totalReturnPrice +
                 '}';
