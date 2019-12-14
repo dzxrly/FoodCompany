9870fb8635acf1b44f63b45a313dba7266aa0bd4
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-14 13:23:50 +0800

    FinancialSheetSearch 查询功能完成 可以根据收入支出表查询

--- src/service/FinancialSheetSearch.java
+++ src/service/FinancialSheetSearch.java
@@ -23,14 +23,13 @@ public class FinancialSheetSearch {
         String hql = "";
         List list = null;
         try {
-            if(index == 0){
+            if (index == 0) {
                 hql = "select i.incomeId,i.incomeType,i.incomePrice,i.incomeTime from Income i where i.incomeId =" + id;
-                list= session.createQuery(hql).list();
-                System.out.println("_________OK!_______________" + (String) ((Object[])list.get(0))[3]);//如果list为空 ((Object[])list.get(0))[3]得不到就会报错
-            }
-            else{
+                list = session.createQuery(hql).list();
+                System.out.println("_________OK!_______________" + (String) ((Object[]) list.get(0))[3]);//如果list为空 ((Object[])list.get(0))[3]得不到就会报错
+            } else {
                 hql = "select paymentId,paymentType,paymentPrice,payTime from Payment where paymentId =" + id;
-                list= session.createQuery(hql).list();
+                list = session.createQuery(hql).list();
             }
 //            FinancialTable f=(FinancialTable) list.get(0);
 //            System.out.println("_________"+f.getDate()+"_______________");
@@ -44,58 +43,80 @@ public class FinancialSheetSearch {
         }
     }
 
-    public void searchAll(int index1, int index2) {// index1=0 0表示仅查询收入表 1表示仅查询支出表  index2=0表示全部 1表示近一个月 2表示近半年 3表示近一年
 
-        Session session= HibernateUtils.openSession();;
-        int ans=0;
-        String hql="";
+    public List searchAll(int index1, int index2) {// index1=0 0表示仅查询收入表 1表示仅查询支出表  index2=0表示全部 1表示近一个月 2表示近半年 3表示近一年
 
-        try{
-            if(index1 == 0){//表示收入表查询全部
-                if(index2==0){//查询全部
-                    hql="from Income";
-                }else if(index2==1){//查询近一个月
+        Session session = HibernateUtils.openSession();
+        String hql = "";
+        List list = null;
+        try {
+            if (index1 == 0) {//表示收入表查询全部
+                if (index2 == 0) {//查询全部
+                    hql = "from Income";
+                } else if (index2 == 1) {//查询近一个月
                     SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                     String nowDate = df.format(new Date()).toString();//获得日期
-                    String startDate =PlusDay.plusDay(-30,nowDate);
+                    String startDate = PlusDay.plusDay(-30, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Income where incomeTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+                } else if (index2 == 2) {//查询近半年
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    String nowDate = df.format(new Date()).toString();//获得日期
+                    String startDate = PlusDay.plusDay(-180, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Income where incomeTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+                } else {//查询近一年
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    String nowDate = df.format(new Date()).toString();//获得日期
+                    String startDate = PlusDay.plusDay(-365, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Income where incomeTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+                }
+            } else {//表示支出表查询全部
+                if (index2 == 0) {
+                    hql = "from Payment ";
 
-                    hql="from Income where incomeTime >= " + startDate;
-                    System.out.println(hql + "________"+startDate);
-                }else if(index2==2){//查询近半年
-
-                }else{//查询近一年
-
+                } else if (index2 == 1) {
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    String nowDate = df.format(new Date()).toString();//获得日期
+                    String startDate = PlusDay.plusDay(-30, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Payment where payTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+                } else if (index2 == 2) {
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    String nowDate = df.format(new Date()).toString();//获得日期
+                    String startDate = PlusDay.plusDay(-180, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Payment where payTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+                } else {
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                    String nowDate = df.format(new Date()).toString();//获得日期
+                    String startDate = PlusDay.plusDay(-365, nowDate);
+                    System.out.println("________" + startDate);
+                    hql = "from Payment where payTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
                 }
             }
-            else{//表示支出表查询全部被
-                if(index2==0){
+            list = session.createQuery(hql).list();
 
-                }else if(index2==1){
-
-                }else if(index2==2){
-
-                }else{
-
+            if (list.toString() != "[]" && list != null) {
+                for (int i = 0; i < list.size(); i++) {
+                    if (index1 == 0) {
+                        Income in = (Income) list.get(i);
+                        System.out.println("______" + in + "________");
+                    } else {
+                        Payment pay = (Payment) list.get(i);
+                        System.out.println("______" + pay + "________");
+                    }
                 }
+            } else {
+                System.out.println("______don't have data________");
             }
-            List list=session.createQuery(hql).list();
-            for(int i=0;i<list.size();i++){
-                if(index1==0){
-                    Income in=(Income) list.get(i);
-                    System.out.println("______"+in+"________");
-                }else{
-                    Payment pay=(Payment) list.get(i);
-                    System.out.println("______"+pay+"________");
-                }
-            }
-
-            ans=1;
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("____________________Can not submit_________________");
             throw e;
-        }finally {
+        } finally {
             session.close();
-            return ;
+            return list;
         }
     }
 }
@@ -104,13 +125,6 @@ public class FinancialSheetSearch {
 //        FinancialSheetSearch fsh=new FinancialSheetSearch();
 //        fsh.searchId("7");
 
-
-//                FinancialTable f= new FinancialTable();
-//                Object[] ob = (Object[]) list.get(0);
-//                f.setId((int) ob[0]);
-//                f.setPrice((double) ob[1]);
-//                f.setDate((String) ob[2]);
-//                f.setType(1);
 
 //
 //    FinancialSheetSearch fsh = new FinancialSheetSearch();
