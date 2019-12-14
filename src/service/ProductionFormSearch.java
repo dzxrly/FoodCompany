2e5c48dff91a4b713e4295c8447c4d65a8c4d220
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-15 02:44:31 +0800

    增加方法searchProductionFormPlanStateEqualZero

--- src/service/ProductionFormSearch.java
+++ src/service/ProductionFormSearch.java
@@ -47,4 +47,48 @@ public class ProductionFormSearch {//生产财务规划界面的部分功能
         }
     }
 
+
+    public List searchProductionFormPlanStateEqualZero(int index){//用于查询planstate状态为0的生产表单
+        Session session= HibernateUtils.openSession();
+        String hql="";
+        List list=null;
+        try{
+            if(index == 0){
+                hql="from ProductionForm where planState = 0";
+            }else if(index ==1){//
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                String nowDate = df.format(new Date()).toString();//获得日期
+                String startDate = PlusDay.plusDay(-30, nowDate);
+                System.out.println("________" + startDate);
+                hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+            }else if(index == 2){
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                String nowDate = df.format(new Date()).toString();//获得日期
+                String startDate = PlusDay.plusDay(-180, nowDate);
+                System.out.println("________" + startDate);
+                hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+            } else{
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+                String nowDate = df.format(new Date()).toString();//获得日期
+                String startDate = PlusDay.plusDay(-365, nowDate);
+                System.out.println("________" + startDate);
+                hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
+            }
+            list = session.createQuery(hql).list();
+        }catch (RuntimeException e){
+            throw e;
+        }
+        finally {
+            session.close();
+            return list;
+        }
+    }
 }
+
+// test searchProductionFormPlanStateEqualZero
+//        ProductionFormSearch p= new ProductionFormSearch();
+//        List list = p.searchProductionFormPlanStateEqualZero(0);
+//        for(int i=0;i<list.size();i++){
+//            ProductionForm pf=(ProductionForm) list.get(i);
+//            System.out.println(pf);
+//        }
