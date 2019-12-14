12e838c4c3ea1bccef611a8ca79dd1452463e583
Abrahamchen <530153667@qq.com>
2019-12-14 21:54:44 +0800

    debug 个人资料修改和密码修改

--- src/service/PersonalInfoGet.java
+++ src/service/PersonalInfoGet.java
@@ -26,20 +26,24 @@ public class PersonalInfoGet {
     }
 
     //个人信息修改（密码和别的信息）返回状态
-    public int stuffInfoUpdate(int stuffNumber,int gender, String name, String phoneNumber, String address, String email) {
+    public int stuffInfoUpdate(int stuffNumber, int gender, String name, String phoneNumber, String address, String email) {
         //状态码为0表示修改成功 1表示修改失败
         int state = 0;
         Session session = HibernateUtils.openSession();
         Transaction tx = null;
         Stuff sf = new Stuff();
         try {
+            tx = session.beginTransaction();
             sf = session.get(Stuff.class, stuffNumber);
             sf.setGender(gender);
             sf.setAddress(address);
             sf.setPersonalName(name);
             sf.setPhoneNumber(phoneNumber);
             sf.setEmail(email);
+            session.update(sf);
+            tx.commit();
         } catch (RuntimeException e) {
+            tx.rollback();
             state=1;
             System.out.println("-------cannot update-------");
             throw e;
@@ -50,17 +54,21 @@ public class PersonalInfoGet {
 
     }
 
-    public int stuffUpdatePsw(int stuffNumber,String oldPsw, String newPsw) {
-        //状态码为0表示修改成功，状态码为1表示密码相同
+    public int stuffUpdatePsw(int stuffNumber, String oldPsw, String newPsw) {
+        //状态码为0表示修改成功，状态码为1表示原密码输入和存的不同
         int state = 0;
         Session session = HibernateUtils.openSession();
         Transaction tx = null;
         Stuff sf = new Stuff();
         try {
+            tx=session.beginTransaction();
             sf = session.get(Stuff.class, stuffNumber);
-            if(sf.getPassword().equals(oldPsw)) state=1;
+            if (!sf.getPassword().equals(oldPsw)) state = 1;
             else sf.setPassword(newPsw);
+            session.update(sf);
+            tx.commit();
         } catch (RuntimeException e) {
+            tx.rollback();
             System.out.println("----------can not update---------");
             throw e;
         } finally {
