cfbfac7c1b4f526121c05b3fe9138590a4f63417
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-16 23:15:20 +0800

    更新了ProduuctionFormSubmission的部分功能 不会有影响
    Signed-off-by: LiDayDay <48319942+LiDayDay@users.noreply.github.com>

--- src/service/ProductionFormSubmission.java
+++ src/service/ProductionFormSubmission.java
@@ -28,7 +28,8 @@ public class ProductionFormSubmission {
 
         try{
             tx=session.beginTransaction();
-            hql="select sum(o.orderQuantity/p.productQuantityPerTime) from OrderBookGoods o,ProductUnit  p where o.goodsNumber = p.goodsId and orderId = " + orderId;
+            //sum(应该生产数量/单位生产时间)
+            hql="select sum(o.producedQuantity/p.productQuantityPerTime) from OrderBookGoods o,ProductUnit  p where o.goodsNumber = p.goodsId and orderId = " + orderId;
             List list = session.createQuery(hql).list();
             Object ob = (Object) list.get(0);
             double userTime = Double.parseDouble(ob.toString())/24;
