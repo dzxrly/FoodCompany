e4b278698a648790dcc3864cd17eda311086ec4a
EggTargaryen <34475415+dzxrly@users.noreply.github.com>
2019-12-17 16:10:31 +0800

    新增生产表单页面前后端交互（有逻辑BUG）

--- src/service/ProductionFormSearch.java
+++ src/service/ProductionFormSearch.java
@@ -10,26 +10,26 @@ import java.util.List;
 
 public class ProductionFormSearch {//生产财务规划界面的部分功能
 
-    public List searchProductionForm(int index){
-        Session session= HibernateUtils.openSession();
-        String hql="";
-        List list=null;
-        try{
-            if(index == 0){
-                hql="from ProductionForm";
-            }else if(index ==1){//
+    public List searchProductionForm(int index) {
+        Session session = HibernateUtils.openSession();
+        String hql = "";
+        List list = null;
+        try {
+            if (index == 0) {
+                hql = "from ProductionForm";
+            } else if (index == 1) {//
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-30, nowDate);
                 System.out.println("________" + startDate);
                 hql = "from ProductionForm where buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
-            }else if(index == 2){
+            } else if (index == 2) {
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-180, nowDate);
                 System.out.println("________" + startDate);
                 hql = "from ProductionForm where buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
-            } else{
+            } else {
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-365, nowDate);
@@ -38,36 +38,35 @@ public class ProductionFormSearch {//生产财务规划界面的部分功能
             }
 
             list = session.createQuery(hql).list();
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw e;
-        }
-        finally {
+        } finally {
             session.close();
             return list;
         }
     }
 
 
-    public List searchProductionFormPlanStateEqualZero(int index){//用于查询planstate状态为0的生产表单
-        Session session= HibernateUtils.openSession();
-        String hql="";
-        List list=null;
-        try{
-            if(index == 0){
-                hql="from ProductionForm where planState = 0";
-            }else if(index ==1){//
+    public List searchProductionFormPlanStateEqualZero(int index) {//用于查询planstate状态为0的生产表单
+        Session session = HibernateUtils.openSession();
+        String hql = "";
+        List list = null;
+        try {
+            if (index == 0) {
+                hql = "from ProductionForm where planState = 0";
+            } else if (index == 1) {//
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-30, nowDate);
                 System.out.println("________" + startDate);
                 hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
-            }else if(index == 2){
+            } else if (index == 2) {
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-180, nowDate);
                 System.out.println("________" + startDate);
                 hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
-            } else{
+            } else {
                 SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
                 String nowDate = df.format(new Date()).toString();//获得日期
                 String startDate = PlusDay.plusDay(-365, nowDate);
@@ -75,10 +74,9 @@ public class ProductionFormSearch {//生产财务规划界面的部分功能
                 hql = "from ProductionForm where planState = 0 and buildTime > date_format('" + startDate + "','%Y-%m-%d %H:%i:%s')";
             }
             list = session.createQuery(hql).list();
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw e;
-        }
-        finally {
+        } finally {
             session.close();
             return list;
         }
