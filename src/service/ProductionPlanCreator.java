a5953861d916ac4d6ddf4fedd98e306cdf1c818e
LiDayDay <48319942+lidayday@users.noreply.github.com>
2019-12-18 18:06:30 +0800

    更新了ProductionPlanCreator的功能 增加了ProductDetailPlan细节表 创建时候要级联创建
    Signed-off-by: LiDayDay <48319942+LiDayDay@users.noreply.github.com>

--- src/service/ProductionPlanCreator.java
+++ src/service/ProductionPlanCreator.java
@@ -15,36 +15,37 @@ import java.util.List;
 
 public class ProductionPlanCreator {//生产计划管理界面
 
-    public ProductPlan createProdcutionPlan(int productionId,int stuffNumber,String startTime,String endTime,String orderId){// 创建生产计划表 更新productionForm的planState置1 更新productionState置1
 
-        Session session= HibernateUtils.openSession();
-        Transaction tx=null;
-        int ans=0;
-        String hql="";
+    public ProductPlan createProductionPlan(int productionId, int stuffNumber, String startTime, String endTime, String orderId) {// 创建生产计划表 更新productionForm的planState置1 更新productionState置1
+        Session session = HibernateUtils.openSession();
+        Transaction tx = null;
+        int ans = 0;
+        String hql = "";
         ProductPlan p = new ProductPlan();
+
         p.setProductionId(productionId);
         p.setStuffNumber(stuffNumber);
         p.setStartTime(startTime);
         p.setEndTime(endTime);
         p.setProductionState(0);
 
+
         SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         String date = dt.format(new Date()).toString();//获得日期
         p.setBuildTime(date);
 
-        if(orderId!="0"){
+        if (orderId != "0") {
             p.setPlanId(0);
-        }
-        else{
+        } else {
             p.setPlanType(1);
         }
-        try{
-            tx=session.beginTransaction();
+        try {
+            tx = session.beginTransaction();
             //生产生产计划表
-            hql="select sum(o.producedQuantity/p.productQuantityPerTime) from OrderBookGoods o,ProductUnit  p where o.goodsNumber = p.goodsId and orderId = " + orderId;
+            hql = "select sum(o.producedQuantity/p.productQuantityPerTime) from OrderBookGoods o,ProductUnit  p where o.goodsNumber = p.goodsId and orderId = " + orderId;
             List list = session.createQuery(hql).list();
             Object ob = (Object) list.get(0);
-            double cycle = Double.parseDouble(ob.toString())/24;
+            double cycle = Double.parseDouble(ob.toString()) / 24;
             DecimalFormat df = new DecimalFormat("#.00");
             cycle = Double.parseDouble(df.format(cycle));
 
@@ -52,25 +53,26 @@ public class ProductionPlanCreator {//生产计划管理界面
             p.setProductionCycle(cycle);
             session.save(p);
             //更新生产表单中的两个状态
-            hql="update ProductionForm set planState = 1,productionState = 1 where productionId = " +productionId;
+            hql = "update ProductionForm set planState = 1,productionState = 1 where productionId = " + productionId;
             Query query = session.createQuery(hql);
             query.executeUpdate();
 
-            ans=1;
+            ans = 1;
             tx.commit();
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("____________________Can not submit_________________");
             ans = 0;
             tx.rollback();
             throw e;
-        }finally {
+        } finally {
             session.close();
             return p;
         }
     }
 
+
     //创建细节表
-    public void createProductionDetailPlan(ProductPlan p, String productionId){//p是刚刚建好的ProductionPlan主表 productionId是productionDetailForm表的 生产编号
+    public void createProductionDetailPlan (ProductPlan p, String productionId) {//p是刚刚建好的ProductionPlan主表 productionId是productionDetailForm表的 生产编号
         Session session=HibernateUtils.openSession();
         Transaction tx =null;
 
